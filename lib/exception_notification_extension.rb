326c576ea1866a5072409ef57bf92ce950314b13
Antarya <antarya@gmail.com>
2015-12-28 16:18:10 +0400

    Creating gemspec and gems code

---
+++ lib/exception_notification_extension.rb
@@ -0,0 +1,80 @@
+require "exception_notification_extension/version"
+
+module ExceptionNotificationExtension
+ def exception_notification(env, exception)
+    super(env, exception)
+    editor = "subl" # subl|atom
+
+    #@env        = env
+    @exception  = exception
+    #@options    = (env['exception_notifier.options'] || {}).reverse_merge(self.class.default_options)
+    @kontroller = env['action_controller.instance'] || MissingController.new
+    @request    = ActionDispatch::Request.new(env)
+    @backtrace  = clean_backtrace(exception)
+    #@sections   = @options[:sections]
+    #data        = env['exception_notifier.exception_data'] || {}
+
+    begin
+      title = "#{@exception.class} in #{@kontroller.controller_name}##{@kontroller.action_name}"
+      output = ""
+
+      output += @exception.message.to_s + "\n" if @exception.message.to_s.present?
+      bs = @backtrace.select{|b| b.include?("app/") }.map{|b| b}
+      output += bs.join("\n")
+
+      pars = []
+      @request.parameters.each {|k, v| 
+        pars.push("#{k}: #{v}") if !["controller", "action"].index(k).present? 
+      }
+      output += "\n{ #{pars.join(', ')} }" if pars.present?
+
+      os_notify(title, output)
+
+      if bs.present?
+        open_file_in_editor(editor, get_path(bs[0])) # tested with atom
+      end
+
+    rescue Exception => e
+      puts "ExceptionNotifierExtensions has some errors #{e.inspect}"
+    end
+
+  end
+  def get_path(path)
+    path.strip!
+    last = path.index(":in")
+    path = path[0, last]
+    first = 0
+
+    for i in (path.length - 1)..0
+      if s[i] == " " 
+        first = i
+        break
+      end
+    end
+    path = path[first, path.length]
+
+    return Rails.root.to_path + "/" + path
+   
+  end
+  def open_file_in_editor(editor, path)
+    puts editor
+    puts path
+    system "#{editor} #{path}"
+  end
+  def os_notify(title, msg)    
+    Notifier.notify(
+      :image   => Rails.root.to_path + "/public/favicon.ico",
+      :title   => title.present? ? title : "Exception Notifier",
+      :message => msg
+    )
+  end
+end
+
+
+
+class ExceptionNotifier
+  class Notifier < ActionMailer::Base
+    prepend ExceptionNotifierExtensions
+  end
+end
+
---
+++ spec/exception_notification_extension_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe ExceptionNotificationExtension do
+  it 'has a version number' do
+    expect(ExceptionNotificationExtension::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
