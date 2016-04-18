7aec052e180215509030178e0e3d2d791b118eed
Antarya <antarya@gmail.com>
2016-04-18 10:41:58 +0400

    For different exception_notification > 4.0 version code was changed, so to reflect this changes two version of code is working based on gem version

--- lib/exception_notification_extension.rb
+++ lib/exception_notification_extension.rb
@@ -1,93 +1,33 @@
-require "exception_notification_extension/version"
+module ExceptionNotifier
+  module EditorHelper
+    def get_path(path)
+      path.strip!
+      last = path.index(":in")
+      path = path[0, last]
+      first = 0
 
-require 'action_mailer'
-require 'exception_notification'
-require 'notifier'
-
-module ExceptionNotificationExtension
- def exception_notification(env, exception)
-
-    super(env, exception)
-
-    @options[:on] = true if !@options.key?(:on)
-    @options[:timeout] = 5000 if !@options.key?(:timeout)
-    #
-    if @options[:on]
-      begin
-        title = "#{@exception.class} in #{@kontroller.controller_name}##{@kontroller.action_name}"
-        output = ""
-
-        output += @exception.message.to_s + "\n" if @exception.message.to_s.present?
-        bs = @backtrace.select{|b| b.include?("app/") }.map{|b| b}
-        output += bs.join("\n")
-
-        pars = []
-        @request.parameters.each {|k, v|
-          pars.push("#{k}: #{v}") if !["controller", "action"].index(k).present?
-        }
-        output += "\n{ #{pars.join(', ')} }" if pars.present?
-
-        os_notify(title, output)
-
-        if bs.present?
-          @options[:editor] = "subl" if !@options.key?(:editor) || ["subl", "atom"].index(@options[:editor]).nil?
-          open_file_in_editor(get_path(bs[0])) # tested with atom
+      for i in (path.length - 1)..0
+        if s[i] == " "
+          first = i
+          break
         end
-
-      rescue Exception => e
-        puts "ExceptionNotifierExtensions has some errors #{e.inspect}"
       end
+      path = path[first, path.length]
+
+      return Rails.root.to_path + "/" + path
+
     end
-  end
-  def get_path(path)
-    path.strip!
-    last = path.index(":in")
-    path = path[0, last]
-    first = 0
-
-    for i in (path.length - 1)..0
-      if s[i] == " "
-        first = i
-        break
-      end
+    def open_file_in_editor(path, options)
+      system "#{options[:editor]} #{path}"
     end
-    path = path[first, path.length]
-
-    return Rails.root.to_path + "/" + path
-
-  end
-  def open_file_in_editor( path)
-    system "#{@options[:editor]} #{path}"
-  end
-  def os_notify(title, msg)
-    Notifier.notify(
-      :image   => Rails.root.to_path + "/public/favicon.ico",
-      :title   => title.present? ? title : "Exception Notifier",
-      :message => msg,
-      :timeout => @options[:timeout]
-    )
-  end
-end
-
-
-if ExceptionNotifier.class == Class
-  class ExceptionNotifier
-    class Notifier < ActionMailer::Base
-      prepend ExceptionNotificationExtension
+    def os_notify(title, msg, options)
+      notify(
+        :image   => Rails.root.to_path + "/public/favicon.ico",
+        :title   => title.present? ? title : "Exception Notifier",
+        :message => msg,
+        :timeout => options[:timeout]
+      )
     end
-  end
-else ExceptionNotifier.class == Module
-  module ExceptionNotifier
-    class Notifier
-      prepend ExceptionNotificationExtension
-    end
-  end
-end
-
-
-
-module Notifier
-  module NotifySend
     def notify(options)
       command = [
         "notify-send", "-i",
@@ -99,6 +39,102 @@ module Notifier
       ]
 
       Thread.new { system(*command) }.join
+    end
+    module_function :get_path, :open_file_in_editor, :os_notify, :notify
+  end
+  ver = Gem.loaded_specs['exception_notification'].version
+  limit_ver = Gem::Version.create('4.0')
+  if ver >= limit_ver
+    class EditorNotifier
+      include ExceptionNotifier::EditorHelper
+      include ExceptionNotifier::BacktraceCleaner
+
+      def initialize(options)
+        @default_options = options
+      end
+
+      def call(exception, options={})
+        env = options[:env]
+        @env        = env
+        @options    = options.reverse_merge(env['exception_notifier.options'] || {}).reverse_merge(@default_options)
+        @kontroller = env['action_controller.instance'] || MissingController.new
+        @request    = ActionDispatch::Request.new(env)
+        @backtrace  = exception.backtrace ? clean_backtrace(exception) : []
+
+        options[:on] = true if !options.key?(:on)
+        options[:timeout] = 5000 if !options.key?(:timeout)
+        if options[:on]
+          begin
+            title = "#{exception.class} in #{@kontroller.controller_name}##{@kontroller.action_name}"
+            output = ""
+            output += exception.message.to_s + "\n" if exception.message.to_s.present?
+            bs = @backtrace.select{|b| b.include?("app/") }.map{|b| b}
+            output += bs.join("\n")
+            pars = []
+            @request.parameters.each {|k, v|
+              pars.push("#{k}: #{v}") if !["controller", "action"].index(k).present?
+            }
+            output += "\n{ #{pars.join(', ')} }" if pars.present?
+            os_notify(title, output, options)
+            if bs.present?
+              options[:editor] = "subl" if !options.key?(:editor) || ["subl", "atom"].index(options[:editor]).nil?
+              open_file_in_editor(get_path(bs[0]), options) # tested with atom
+            end
+          rescue Exception => e
+            puts "ExceptionNotifierExtensions has some errors #{e.inspect}"
+          end
+        end
+      end
+    end
+  else
+    require "exception_notification_extension/version"
+
+    require 'action_mailer'
+    require 'exception_notification'
+    require 'notifier'
+
+    module ExceptionNotificationExtension
+      include ExceptionNotifier::EditorHelper
+
+      def exception_notification(env, exception)
+
+        super(env, exception)
+
+        @options[:on] = true if !@options.key?(:on)
+        @options[:timeout] = 5000 if !@options.key?(:timeout)
+        #
+        if @options[:on]
+          begin
+            title = "#{@exception.class} in #{@kontroller.controller_name}##{@kontroller.action_name}"
+            output = ""
+
+            output += @exception.message.to_s + "\n" if @exception.message.to_s.present?
+            bs = @backtrace.select{|b| b.include?("app/") }.map{|b| b}
+            output += bs.join("\n")
+
+            pars = []
+            @request.parameters.each {|k, v|
+              pars.push("#{k}: #{v}") if !["controller", "action"].index(k).present?
+            }
+            output += "\n{ #{pars.join(', ')} }" if pars.present?
+
+            os_notify(title, output, @options)
+
+            if bs.present?
+              @options[:editor] = "subl" if !@options.key?(:editor) || ["subl", "atom"].index(@options[:editor]).nil?
+              open_file_in_editor(get_path(bs[0]), @options) # tested with atom
+            end
+
+          rescue Exception => e
+            puts "ExceptionNotifierExtensions has some errors #{e.inspect}"
+          end
+        end
+      end
+    end
+    class ExceptionNotifier
+      class Notifier < ActionMailer::Base
+        prepend ExceptionNotificationExtension
+      end
     end
   end
 end
